1554f2dd7eea249be5967718ce24f89758e59ed6
blackmwana <blackmwana@gmail.com>
2013-04-17 13:09:35 -0400

    gallery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
-    var BlogEntry = StackMob.Model.extend({ schemaName: 'blogentry' });
-var entry = new BlogEntry({ message: 'hello world!' });
-entry.create();
+    var user = StackMob.ser() ;
     });
